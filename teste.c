ec91ee55a066ff75bc9b0834968775483c6bb623
27Nizzo <afonso7557@gmail.com>
2024-04-07 12:44:22 +0100

    teste

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    printf("Insert a number: ");
+    scanf("%d", &n);
+    if(n == 1) {
+    printf("My ball's are heavy :P");
+    } else {
+        printf("I got ball's in my jaw");
+    }
+    return 0; 
+}
